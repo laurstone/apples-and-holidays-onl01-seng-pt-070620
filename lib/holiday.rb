b60e595d2b55d59a3d398c4681c6fd31a4c97887
Lauren Stone <lstone321@gmail.com>
2020-07-21 19:31:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
+      binding.pry
     puts "  #{event.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{supplies.join(", ")}"
     end
   end
